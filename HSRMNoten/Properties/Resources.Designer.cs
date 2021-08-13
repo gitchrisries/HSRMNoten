8a5990ca1b6a635b96bc821c79f13e26b69c02ce
gitchrisries <88830608+gitchrisries@users.noreply.github.com>
2021-08-13 09:31:21 +0200

    Update

--- HSRMNoten/Properties/Resources.Designer.cs
+++ HSRMNoten/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace HSRMNoten.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon tray_icon {
+            get {
+                object obj = ResourceManager.GetObject("tray_icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
